9d5aff9652d817de8ec2b2f68255a5a70cbbaf59
Charan <charankulal0241@gmail.com>
2024-05-04 20:34:44 +0530

    Create get employee by id REST API.

--- ems-backend/src/main/java/com/charan/emsbackend/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/charan/emsbackend/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.charan.emsbackend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -23,5 +20,13 @@ public class EmployeeController {
     {
         EmployeeDto saveEmployee=employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(saveEmployee, HttpStatus.CREATED);
+    }
+
+    // Get employee by ID REST API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId)
+    {
+        EmployeeDto employeeDto=employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
     }
 }
--- ems-backend/src/main/java/com/charan/emsbackend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/charan/emsbackend/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.charan.emsbackend.service.impl;
 
 import com.charan.emsbackend.dto.EmployeeDto;
 import com.charan.emsbackend.entity.Employee;
+import com.charan.emsbackend.exception.ResourceNotFoundException;
 import com.charan.emsbackend.mapper.EmployeeMapper;
 import com.charan.emsbackend.repository.EmployeeRepository;
 import com.charan.emsbackend.service.EmployeeService;
@@ -19,5 +20,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee= EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee=employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee=employeeRepository.findById(employeeId).orElseThrow(()-> new ResourceNotFoundException("Employee with "+employeeId+" does not exist"));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
