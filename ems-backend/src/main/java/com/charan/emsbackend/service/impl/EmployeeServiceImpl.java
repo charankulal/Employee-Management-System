c9872d5bf8dfca3051b55f919566eaeffc99d425
Charan <charankulal0241@gmail.com>
2024-05-04 23:51:14 +0530

    Created update employee REST API

--- ems-backend/src/main/java/com/charan/emsbackend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/charan/emsbackend/service/impl/EmployeeServiceImpl.java
@@ -36,4 +36,16 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Employee> employees=employeeRepository.findAll();
         return employees.stream().map(EmployeeMapper::mapToEmployeeDto).collect(Collectors.toList());
     }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(()-> new ResourceNotFoundException("Employee with "+employeeId+ " does not exist"));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+        Employee updatedEmployeeObj=employeeRepository.save(employee);
+
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
 }
