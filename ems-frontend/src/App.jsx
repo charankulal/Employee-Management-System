3931ea9f8e01a90c838333764ceb458f79853ed0
Charan <charankulal0241@gmail.com>
2024-05-16 22:01:44 +0530

    "Added react-router-dom dependency and implemented routing in App.jsx"

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,13 +2,22 @@ import "./App.css";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <>
-  <HeaderComponent></HeaderComponent>
-  <ListEmployeeComponent/>
-  <FooterComponent></FooterComponent>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <HeaderComponent></HeaderComponent>
+        <Routes>
+          {/* <Route path="/" element={<ListEmployeeComponent />}></Route> */}
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>
+
+        <FooterComponent></FooterComponent>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
