575916faf9cd2752e905df1e7ad233fe4f01afb4
Charan <charankulal0241@gmail.com>
2024-05-18 17:11:29 +0530

    Created Add employee form

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,12 +1,75 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from "react";
 
 const EmployeeComponent = () => {
-  return (
-    <div>
-      <h1>Add an Employee</h1>
-    </div>
-  )
-}
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
 
-export default EmployeeComponent
+  function saveEmployee(e)
+  {
+    e.preventDefault();
+    const employee ={firstName, lastName, email}
+    console.log(employee)
+  }
+
+  return (
+    <div className="container my-4">
+      <div className="row">
+        <div className="card col-6 offset-3">
+          <h2 className="text-center">Add Employee</h2>
+          <div className="card-body">
+            <form action="">
+              <div className="form-group mb-2">
+                <label htmlFor="firstName" className="form-label">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Employee Firstname"
+                  name="firstName"
+                  value={firstName}
+                  className="form-control"
+                  onChange={(e)=> setFirstName(e.target.value) }
+                />
+              </div>
+
+              <div className="form-group mb-2">
+                <label htmlFor="lastName" className="form-label">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Employee LastName"
+                  name="lastName"
+                  value={lastName}
+                  className="form-control"
+                  onChange={(e)=> setLastName(e.target.value) }
+                />
+              </div>
+
+              <div className="form-group mb-2">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Employee Email Id"
+                  name="email"
+                  value={email}
+                  className="form-control"
+                  onChange={(e)=> setEmail(e.target.value) }
+                />
+              </div>
+              <div className="form-group text-center m-4">
+              <button className="btn btn-success" onClick={saveEmployee}>Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeComponent;
